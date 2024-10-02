f01fe11ebd112888764220127815473cc153f464
sachinjangam29 <Sachinjagannathjangam29@gmail.com>
2024-10-02 21:49:44 +0530

    added react-router

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,14 +4,25 @@ import './App.css';
 import {Footer} from "./layouts/NavbarAndFooters/Footer";
 import {Homepage} from "./layouts/Homepage/Homepage";
 import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
+import {Redirect, Route, Switch} from "react-router-dom";
 
-export const  App = () => {
-  return (
-      <div>
-        <Navbar/>
-       {/* <Homepage/>*/}
-          <SearchBooksPage/>
-        <Footer/>
-      </div>
-  );
+export const App = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Switch>
+                <Route path='/' exact>
+                   <Redirect to ='/home' />
+                </Route>
+                <Route path='/home'>
+                    <Homepage/>
+                </Route>
+                <Route path='/search'>
+                    <SearchBooksPage/>
+                </Route>
+            </Switch>
+
+            <Footer/>
+        </div>
+    );
 }
